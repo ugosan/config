f8c2ca80dc0765ca38fbecb4cfe46d242364781c
Ugo Sangiorgi <ugo.sangiorgi@gmail.com>
2019-01-21 15:02:02 -0500

    configs

---
+++ README.md
@@ -0,0 +1,16 @@
+
+### Basic Installation
+
+Configure via the command-line with either `curl` or `wget`.
+
+#### via curl
+
+```shell
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ugosan/config/master/config-centos.sh)"
+```
+
+#### via wget
+
+```shell
+sh -c "$(wget https://raw.githubusercontent.com/ugosan/config/master/config-centos.sh -O -)"
+```
--- config-centos.sh
+++ config-centos.sh
@@ -2,4 +2,14 @@
 
 echo "Configuring CentOS"
 
-yum install git zsh -y
+yum install -y git zsh vim curl
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/ugosan/config.git /tmp/ugosan-config
+cp /tmp/ugosan-config/ugo.zsh-theme ~/.oh-my-zsh/themes/ugo.zsh-theme
+cp /tmp/vim/vimrc ~/.vimrc
+
+sed '/ZSH_THEME/ s/^#*/#/' ~/.oh-my-zsh > ~/.oh-my-zsh
+echo 'ZSH_THEME="ugo"' >> ~/.oh-my-zsh
+echo 'alias ll="ls -lah"' >> ~/.oh-my-zsh
