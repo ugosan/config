6458877f922bc4f18725ca47c71b9ac669970960
Ugo Sangiorgi <ugo.sangiorgi@gmail.com>
2019-01-21 15:06:05 -0500

    update config

--- config-centos.sh
+++ config-centos.sh
@@ -6,6 +6,10 @@ yum install -y git zsh vim curl
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+
 git clone https://github.com/ugosan/config.git /tmp/ugosan-config
 cp /tmp/ugosan-config/ugo.zsh-theme ~/.oh-my-zsh/themes/ugo.zsh-theme
 cp /tmp/vim/vimrc ~/.vimrc
@@ -13,3 +17,5 @@ cp /tmp/vim/vimrc ~/.vimrc
 sed '/ZSH_THEME/ s/^#*/#/' ~/.oh-my-zsh > ~/.oh-my-zsh
 echo 'ZSH_THEME="ugo"' >> ~/.oh-my-zsh
 echo 'alias ll="ls -lah"' >> ~/.oh-my-zsh
+
+chsh -s /bin/zsh
