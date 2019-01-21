8b1deb028915011986577e1a832d438f73a54793
Ugo Sangiorgi <ugo.sangiorgi@gmail.com>
2019-01-21 18:36:03 -0500

    fix

--- config-centos.sh
+++ config-centos.sh
@@ -5,14 +5,11 @@ echo "Configuring CentOS"
 yum install -y git zsh vim curl
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
-cp -i ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-
-
 git clone https://github.com/ugosan/config.git /tmp/ugosan-config
-cp -i /tmp/ugosan-config/zsh/ugo.zsh-theme ~/.oh-my-zsh/themes/ugo.zsh-theme
-cp -i /tmp/ugosan-config/zsh/zshrc ~/.zshrc
-cp -i /tmp/ugosan-config/vim/vimrc ~/.vimrc
 
+yes | cp -rf ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+yes | cp -rf /tmp/ugosan-config/zsh/ugo.zsh-theme ~/.oh-my-zsh/themes/ugo.zsh-theme
+yes | cp -rf /tmp/ugosan-config/zsh/zshrc ~/.zshrc
+yes | cp -rf /tmp/ugosan-config/vim/vimrc ~/.vimrc
 
 chsh -s /bin/zsh
