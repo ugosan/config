bf4dbfab4eac6691402d5e5b1afa0e4a127af0d9
Ugo Sangiorgi <ugo.sangiorgi@gmail.com>
2019-01-21 15:20:55 -0500

    cp -i

--- config-centos.sh
+++ config-centos.sh
@@ -6,13 +6,13 @@ yum install -y git zsh vim curl
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+cp -i ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 
 git clone https://github.com/ugosan/config.git /tmp/ugosan-config
-cp /tmp/ugosan-config/zsh/ugo.zsh-theme ~/.oh-my-zsh/themes/ugo.zsh-theme
-cp /tmp/ugosan-config/zsh/zshrc ~/.zshrc
-cp /tmp/ugosan-config/vim/vimrc ~/.vimrc
+cp -i /tmp/ugosan-config/zsh/ugo.zsh-theme ~/.oh-my-zsh/themes/ugo.zsh-theme
+cp -i /tmp/ugosan-config/zsh/zshrc ~/.zshrc
+cp -i /tmp/ugosan-config/vim/vimrc ~/.vimrc
 
 
 chsh -s /bin/zsh
