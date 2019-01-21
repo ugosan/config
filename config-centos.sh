fd9884673bf4f78ff69a329e487bff689eeef44b
Ugo Sangiorgi <ugo.sangiorgi@gmail.com>
2019-01-21 15:11:06 -0500

    fix

--- config-centos.sh
+++ config-centos.sh
@@ -12,8 +12,8 @@ git clone https://github.com/ugosan/config.git /tmp/ugosan-config
 cp /tmp/ugosan-config/zsh/ugo.zsh-theme ~/.oh-my-zsh/themes/ugo.zsh-theme
 cp /tmp/ugosan-config/vim/vimrc ~/.vimrc
 
-sed '/ZSH_THEME/ s/^#*/#/' ~/.oh-my-zsh > ~/.oh-my-zsh
-echo 'ZSH_THEME="ugo"' >> ~/.oh-my-zsh
-echo 'alias ll="ls -lah"' >> ~/.oh-my-zsh
+sed '/ZSH_THEME/ s/^#*/#/' ~/.zshrc > ~/.zshrc
+echo 'ZSH_THEME="ugo"' >> ~/.zshrc
+echo 'alias ll="ls -lah"' >> ~/.zshrc
 
 chsh -s /bin/zsh
