6fa628f09649d3b545d17ff50cbfdac1a5b76b1b
Ugo Sangiorgi <ugo.sangiorgi@gmail.com>
2019-01-21 15:07:05 -0500

    updates

--- config-centos.sh
+++ config-centos.sh
@@ -4,8 +4,6 @@ echo "Configuring CentOS"
 
 yum install -y git zsh vim curl
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
